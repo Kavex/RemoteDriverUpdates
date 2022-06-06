1f9cbe55683246548b15c2cd88e12e81abbef355
Kerrigan <kavex1988@gmail.com>
2022-06-06 13:17:22 -0600

    Update and rename RunDellUpdates.bat to RunDCU_Updates.bat

--- Dell/RunDCU_Updates.bat
+++ Dell/RunDCU_Updates.bat
@@ -1,3 +1,2 @@
 cd C:\Program Files (x86)\Dell\CommandUpdate
 dcu-cli.exe /applyUpdates -silent -updateType=bios,firmware,driver -autoSuspendBitLocker=enable -outputLog=C:\Temp\DCU_Log.log
-shutdown -r -t 36000
