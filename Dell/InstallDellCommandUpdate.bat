9867e40d78f241a51f90157d3682aa46fa889d0f
Kerrigan <kavex1988@gmail.com>
2022-11-08 11:13:11 -0700

    Update InstallDellCommandUpdate.bat

--- Dell/InstallDellCommandUpdate.bat
+++ Dell/InstallDellCommandUpdate.bat
@@ -1,5 +1,5 @@
-REM https://github.com/Kavex/RemoteDriverUpdates/releases/tag/4.6
+REM https://github.com/Kavex/RemoteDriverUpdates
 
 cd c:\temp\
-echo | powershell Invoke-WebRequest -Uri "https://github.com/Kavex/RemoteDriverUpdates/releases/download/4.6/Dell-Command-Update-Windows-Universal-Application_HG8CC_WIN_4.6.0_A00.EXE" -OutFile Dell-Command-Update-Windows-Universal-Application_HG8CC_WIN_4.6.0_A00.EXE
-Dell-Command-Update-Windows-Universal-Application_HG8CC_WIN_4.6.0_A00.EXE /s
+echo | powershell Invoke-WebRequest -Uri "https://github.com/Kavex/RemoteDriverUpdates/releases/download/4.7/Dell-Command-Update-Windows-Universal-Application_JY04X_WIN_4.7.0_A00_01.EXE" -OutFile Dell-Command-Update-Windows-Universal-Application_JY04X_WIN_4.7.0_A00_01.EXE
+Dell-Command-Update-Windows-Universal-Application_JY04X_WIN_4.7.0_A00_01.EXE /s
