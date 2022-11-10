057724c9c6c1a14bda39e75309df31a2dbb46ca0
Kerrigan <kavex1988@gmail.com>
2022-11-10 10:40:22 -0700

    Update HP_Image_Assistant.bat

--- HP/HP_Image_Assistant.bat
+++ HP/HP_Image_Assistant.bat
@@ -3,4 +3,6 @@ cd c:\temp\
 echo | powershell Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.1.6.exe" -OutFile hp-hpia-5.1.6.exe
 hp-hpia-5.1.6.exe /s
 cd C:\SWSetup\sp142446
-HPImageAssistant.exe /Action:Install /AutoCleanup /Category:BIOS,Drivers,Firmware /Silent
+HPImageAssistant.exe /Action:Install /AutoCleanup /Category:BIOS,Drivers,Firmware /Silent /AutoReport
+
+REM Driver Install Logs: %ProgramData%\HP\HP Image Assistant\
