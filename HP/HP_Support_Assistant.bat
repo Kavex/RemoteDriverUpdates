c2ad69e71572dafe3a78f5ccc997d19531de4834
Kerrigan <kavex1988@gmail.com>
2022-11-10 10:44:07 -0700

    Add files via upload

--- HP/HP_Support_Assistant.bat
+++ HP/HP_Support_Assistant.bat
@@ -1,6 +1,10 @@
+REM HP Support Assistant 9 
 REM https://support.hp.com/us-en/help/hp-support-assistant
+
+REM Legacy Version: https://ftp.hp.com/pub/softpaq/sp114001-114500/sp114036.exe
+
 cd c:\temp\
-echo | powershell Invoke-WebRequest -Uri "https://ftp.ext.hp.com/pub/softpaq/sp140001-140500/sp140482.exe" -OutFile sp140482.exe
-sp140482.exe /s
-cd C:\SWSetup\sp140482
+echo | powershell Invoke-WebRequest -Uri "https://ftp.ext.hp.com/pub/softpaq/sp142501-143000/sp142943.exe" -OutFile sp142943.exe
+sp142943.exe /s
+cd C:\SWSetup\sp142943
 InstallHPSA.exe /S /v /qn
