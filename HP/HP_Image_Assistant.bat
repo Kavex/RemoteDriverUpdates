a6eb476ef9dd7036c0e6964cd27a0459f04a872b
Kerrigan <kavex1988@gmail.com>
2023-01-13 15:36:17 -0700

    Add files via upload

--- HP/HP_Image_Assistant.bat
+++ HP/HP_Image_Assistant.bat
@@ -1,8 +1,8 @@
 REM http://ftp.ext.hp.com//pub/caps-softpaq/cmit/HPIA.html
 cd c:\temp\
-echo | powershell Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.1.6.exe" -OutFile hp-hpia-5.1.6.exe
-hp-hpia-5.1.6.exe /s
-cd C:\SWSetup\sp142446
+echo | powershell Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/hpia/hp-hpia-5.1.7.exe" -OutFile hp-hpia-5.1.7.exe
+hp-hpia-5.1.7.exe /s
+cd C:\SWSetup\sp143766
 HPImageAssistant.exe /Action:Install /AutoCleanup /Category:BIOS,Drivers,Firmware /Silent /AutoReport
 
-REM Driver Install Logs: %ProgramData%\HP\HP Image Assistant\
+REM Install Logs: %ProgramData%\HP\HP Image Assistant\
